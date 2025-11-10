86414ab9b73e976efef4607b2e71e7c0c00d0c04
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-11-10 19:57:46 -0300

    Criação de arquivos base, incluindo pastas './main/' e './test/', criação de arquivo principal e arquivo teste inicial com estrutura em iostream e include de raylib

---
+++ main/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "raylib.h"
+using namespace std;
+// compile using '$ g++ main.cpp -lraylib'
+
+int main(){
+
+    while (!WindowShouldClose()){ // execute the framerates inside this loop
+
+    }
+
+    CloseWindow();
+    return 0;
+}
---
+++ test/basestucture.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "raylib.h"
+using namespace std;
+// compile using '$ g++ main.cpp -lraylib'
+
+int main(){
+
+    while (!WindowShouldClose()){
+        
+
+    }
+
+    CloseWindow();
+    return 0;
+}
